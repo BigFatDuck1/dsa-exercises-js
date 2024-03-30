5b32623f620fb1e1dd5431e75a7449218f450811
BigFatDuck1 <clementho999@gmail.com>
2024-03-30 23:22:36 +0800

    Finished sumAllNumbers.js

--- Recursion/sumAllNumbers.js
+++ Recursion/sumAllNumbers.js
@@ -3,3 +3,14 @@
 
 // Sample: sumRange(3) returns 6, since 1 + 2 + 3 = 6.
 
+function sumRange(int) {
+    if (int == 1) {
+        return 1;
+    }
+    else {
+        return int + sumRange(int - 1);
+    }
+}
+
+let test = sumRange(3)
+console.log(test);
