ef3554119da6e8ad93460ff39c163ba703dd0903
Clement <clementho999@gmail.com>
2024-04-02 01:36:39 +0800

    Completed arrayCheck.js

--- Recursion/arrayCheck.js
+++ Recursion/arrayCheck.js
@@ -5,11 +5,25 @@
 //If all the values in the array "fit the criteria" of the function, arrayCheck() returns true, 
 //otherwise, false
 function arrayCheck(array, func) {
+	//Base
+	if (array.length == 0) {
+		return true;
+	}
+
+	//Recursive
+	if (func(array[0]) == true) {
+		array.shift();
+		console.log(array);
+		return arrayCheck(array, func);
+	}
+	else {
+		return false;
+	}
 
 }
 
 //Test
-let allAreLessThanSeven = arrayCheck([1,2,9], function(num){
+let allAreLessThanSeven = arrayCheck([1,2,6,10], function(num){
 	return num < 7;
 });
 
