1d2f2b584c6c6725003980a431af17dc5544fdd0
BigFatDuck1 <clementho999@gmail.com>
2024-04-02 20:25:36 +0800

    Completed productArray.js

--- Recursion/productArray.js
+++ Recursion/productArray.js
@@ -1,9 +1,19 @@
 // Write a function called productOfArray which takes in an array of numbers and returns the product of them all
 
 function productOfArray(array) {
-    
+    if (array.length == 1) {
+        return array[0];
+    }
+    else {
+        let first = array[0];
+        array.shift();
+        return first * productOfArray(array);
+    }
 }
 
 
 let six = productOfArray([1,2,3]) // 6
 let sixty = productOfArray([1,2,3,10]) // 60
+
+console.log(six);
+console.log(sixty);
