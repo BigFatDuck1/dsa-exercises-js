58ee73b1362b97fa278b13583491e004be83c8ab
BigFatDuck1 <clementho999@gmail.com>
2024-04-04 00:12:35 +0800

    Added multiarray.js, just started

---
+++ Recursion/multiarray.js
@@ -0,0 +1,7 @@
+// Given a multi-dimensional integer array, return the total number of integers stored inside this array
+
+
+
+//Test
+let seven = totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // 7
+console.log(seven);
