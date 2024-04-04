5d3f9edc61bd239fd0f9a98c0c66a6bd867d69f5
Clement <clementho999@gmail.com>
2024-04-05 02:10:10 +0800

    Added solution that does not use any for loops

--- Recursion/multiarray.js
+++ Recursion/multiarray.js
@@ -21,9 +21,32 @@ function totalIntegers(array) {
 
 }
 
+function cleanerSolution(array) {
+    
+    let counter = 0;
+    //Deal with empty arrays
+    if (array.length == 0) {
+        return 0; //There's nothing inside!
+    }
+    
+    //Using shiftt again...
+    let first_element = array.shift();
+
+    //Recursive case: if it is an array, call the function again
+    if (typeof first_element == "object") {
+        counter += cleanerSolution(first_element);
+    }
+    else if (typeof first_element == "number") {
+        counter += 1;
+    }
+    //Always call itself again (recurse), because you just removed (shift()) the first element from the array
+    return counter + cleanerSolution(array);
+
+}
+
 //Test
 let seven =  totalIntegers([[[5], 3], 0, 2, ['foo'], [], [4, [5, 6]]]); // 7
 console.log(seven);
 
-// let result = totalIntegers([1,2,"foo", [1]]);
-// console.log(result);
+let result = cleanerSolution([1,2,"foo", [1]]); //3
+console.log(result);
