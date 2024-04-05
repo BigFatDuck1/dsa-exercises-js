a7643228c7b907b4a4f985efab678b7c2e117886
Clement <clementho999@gmail.com>
2024-04-05 13:08:44 +0800

    Added sumSquares.js

---
+++ Recursion/sumSquares.js
@@ -0,0 +1,19 @@
+// Write a function that sums squares of numbers in list that may contain more lists
+
+function sumSquares(array) {
+    
+}
+
+
+//Test
+let l = [1,2,3]; 
+console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+
+l = [[1,2],3]; 
+console.log(SumSquares(l)); // 1 + 4 + 9 = 14
+
+l = [[[[[[[[[1]]]]]]]]] 
+console.log(SumSquares(l)); // 1 = 1
+
+l = [10,[[10],10],[10]] 
+console.log(SumSquares(l)); // 100 + 100 + 100 + 100 = 400
