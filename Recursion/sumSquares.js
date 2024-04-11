fd55ae66511c94f47d81dd5aaf9b4b6522c285ca
BigFatDuck1 <clementho999@gmail.com>
2024-04-11 09:58:29 +0800

    Completed sumSquares.js

--- Recursion/sumSquares.js
+++ Recursion/sumSquares.js
@@ -3,37 +3,34 @@
 function sumSquares(array) {
 
     let sum = 0;
-    //Exit if empty array
+
+    //Exit case
     if (array.length == 0) {
         return 0;
     }
-    let first_element = array[0];
-    console.log(first_element);
-    //Base case
-    if (typeof first_element == "number") {
-        sum += first_element * first_element;
-    }
-    //Recursive case
-    else if (typeof first_element == "object") {
-        sumSquares(first_element);
-    }
 
-    array.shift();
-    console.log(array);
-    return sum + sumSquares(array);
+    array.forEach(element => {
+        if (Array.isArray(element) == true) {
+            sum += + sumSquares(element);
+        }
+        else if (typeof element == "number") {
+            sum += element * element;
+        }
+    });
+    return sum;
 
 }
 
 
 //Test
-//let l = [1,2,3]; 
-//console.log(sumSquares(l)); // 1 + 4 + 9 = 14
+let l = [1,2,3]; 
+console.log(sumSquares(l)); // 1 + 4 + 9 = 14
 
 l = [[1,2],3]; 
 console.log(sumSquares(l)); // 1 + 4 + 9 = 14
 
-//l = [[[[[[[[[1]]]]]]]]] 
-//console.log(sumSquares(l)); // 1 = 1
+l = [[[[[[[[[1]]]]]]]]] 
+console.log(sumSquares(l)); // 1 = 1
 
-//l = [10,[[10],10],[10]] 
-//console.log(sumSquares(l)); // 100 + 100 + 100 + 100 = 400
+l = [10,[[10],10],[10]] 
+console.log(sumSquares(l)); // 100 + 100 + 100 + 100 = 400
