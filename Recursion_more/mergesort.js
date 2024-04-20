072c2145040aea3ee36b070b09b8221d5be3b05a
BigFatDuck1 <clementho999@gmail.com>
2024-04-20 13:45:10 +0800

    Added mergesort.js

---
+++ Recursion_more/mergesort.js
@@ -0,0 +1,11 @@
+
+function mergeSort(array) {
+
+    //If array only has one element, it is already sorted
+    if (array.length == 1) {
+        return array;
+    }
+}
+
+let one_element = mergeSort([1]);
+console.log(one_element);
