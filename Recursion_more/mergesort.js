30f02e3d514a773904726d88df19443e1a5308aa
BigFatDuck1 <clementho999@gmail.com>
2024-05-06 16:26:25 +0800

    Rewriting mergesort so it is just one function that is recursive instead of composed of two different functions (merge and sort)

--- Recursion_more/mergesort.js
+++ Recursion_more/mergesort.js
@@ -3,85 +3,38 @@
 
 function mergeSort(array) {
 
+    //Base case
     //If array only has one element, it is already sorted
-    if (array.length == 1) {
+    if (array.length == 1 || array.length == 0) {
         return array;
     }
-    else {
-        let result = split(array);
-        return result;
+    
+    //Recursive case
+    //Split first
+    let middle_point;
+
+    if (array.length % 2 == 0 ) { //even
+        middle_point = array.length / 2;
+    }
+    else if (array.length % 2 == 1) { //odd
+        middle_point = (array.length + 1) / 2;
     }
 
-    function split(arr) {
-        if (arr.length > 1) {
-            
-            let left = [];
-            let right = [];
+    let l = array.slice(0, middle_point);
+    let r = array.slice(middle_point);
 
-            if (arr.length % 2 == 0) { //Divisible by 2 i.e. even
-                let halfway_point = arr.length / 2;
-                for (let i = 0; i < halfway_point; i++) {
-                    left.push(arr[i]);
-                }
-                for (let j = halfway_point; j < arr.length; j++) {
-                    right.push(arr[j]);
-                }
+    let left = mergeSort(l); //if l is just one element, then it will return that element because it is already maximally split
+    let right = mergeSort(r);
 
-                return [left, right];
-            } 
-            else if (arr.length % 2 == 1) { //Odd
-                let halfway_point = (arr.length + 1) / 2;
-                for (let i = 0; i < halfway_point; i++) {
-                    left.push(arr[i]);
-                }
-                for (let j = halfway_point; j < arr.length; j++) {
-                    right.push(arr[j]);
-                }
-
-                return [left, right];
-            } 
-            else if(arr.length == 1) { //Only one element left
-                return "Split";
-            }
-        } 
-    }
-
-    function merge(array1, array2, merged_array) {
-        
-        //1. If either array is empty, push all the values into merged_array
-        //also this is base case
-        if (array1.length == 0 || array2.length == 0) {
-            for (let i of array1) {
-                merged_array.push(i);
-            }
-            for (let j of array2) {
-                merged_array.push(j);
-            }
-        }
-        //2. If non-empty array, take smallest value
-        else {
-            //If left hand side smaller...
-            if (array1[0] < array2[0]) {
-                merged_array.push(array1.shift());
-            }
-            //If right hand side smaller
-            else if (array2[0] < array1[0]) {
-                merged_array.push(array2.shift());
-            }
-            //Recursive call
-            merge(array1, array2, merged_array);
-        }
-
-        return merged_array;
-    }
+    return [...left, right];
 
 }
 
-let one_element = mergeSort([1]);
-console.log(one_element);
+// let one_element = mergeSort([1]);
+// console.log(one_element);
 
-let eight = mergeSort([1,2,3,4,5,6,7,8]);
-console.log(eight);
+// let eight = mergeSort([1,2,3,4,5,6,7,8]);
+// console.log(eight);
 
 let five = mergeSort([1,2,3,4,5]);
 console.log(five);
