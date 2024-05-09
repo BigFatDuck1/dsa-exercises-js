e1aa2560ee732faaf998bb7ad3e16ec7d3c2c10b
BigFatDuck1 <clementho999@gmail.com>
2024-05-09 11:18:04 +0800

    Can add a first node

--- Linked_list/linkedList.js
+++ Linked_list/linkedList.js
@@ -3,7 +3,29 @@
 class LinkedList {
 
     constructor() {
+        this.firstNode = null;
+        this.size = 0;
+    }
 
+    //Append: add node to end of list
+    append(value) {
+        let newNode = new Node();
+        //By default, all newly inserted nodes do not point to a next node because there is no next node
+        newNode.setValue(value);
+        //Set first node if it doesn't exist
+        if (this.firstNode == null) {
+            this.firstNode = newNode;
+            this.size += 1;
+        }
+        //Find last node and make it point to this node
+    }
+
+    size() {
+        return this.size;
+    }
+
+    head() {
+        return this.firstNode;
     }
 
 }
@@ -15,4 +37,19 @@ class Node {
         this.nextNode = null;
     }
 
+
+    setValue(value) {
+        this.value = value;
+    }
+    
+    setPointTo(node) {
+        this.nextNode = node;
+    }
 }
+
+let new_list = new LinkedList;
+
+new_list.append("Lemon");
+
+let first = new_list.head();
+console.log(first);
