83f495d707ae8d199439c2878cad2ba5bc95bb38
BigFatDuck1 <clementho999@gmail.com>
2024-05-13 11:54:40 +0800

    Added twosum.js

---
+++ HashMap/twosum.js
@@ -0,0 +1,6 @@
+//Return the indices of the two numbers in an unsorted list that adds up to a target value 
+//You can assume that only one pair adds up to the target number, and you may not use the same number in the list twice
+
+//Slower solution: O(n^2) time
+
+//Faster solution: O(n) time
