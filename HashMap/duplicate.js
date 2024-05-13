93010a64e84db3392efa6e372c20b3d3657f99e4
BigFatDuck1 <clementho999@gmail.com>
2024-05-14 02:01:32 +0800

    Initial solution takes O(2n) time because there are 2 for loops

--- HashMap/duplicate.js
+++ HashMap/duplicate.js
@@ -1,4 +1,34 @@
 // Given a string, remove all duplicates words. For example, given the string
-// "I am a self-taught programmer looking for a job as a programmer.", 
+// "I am a self-taught programmer looking for a job as a programmer", 
 //your function should return
-// "I am a self-taught programmer looking for a job as a.".
+// "I am a self-taught programmer looking for a job as a".
+
+function duplicate(string) {
+    let split = string.split(' ');
+
+    let hash = {};
+
+    let k = 0;
+
+    for (let individual of split) {
+        if (individual in hash) {
+            
+        }
+        else {
+            hash[individual] = k; 
+            k += 1;
+        }
+    }
+
+    let new_string = "";
+
+    for (i in hash) {
+        new_string += i + " ";
+    }
+
+    return new_string;
+}
+
+//Test
+let result = duplicate("I am a self-taught programmer looking for a job as a programmer");
+console.log(result);
