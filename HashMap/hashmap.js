bf5425004887ac24fab40b3f8663bcc6f11b6e4c
BigFatDuck1 <clementho999@gmail.com>
2024-05-14 17:02:39 +0800

    Started on set() but need to work on handling collision

--- HashMap/hashmap.js
+++ HashMap/hashmap.js
@@ -4,6 +4,7 @@ class HashMap {
     constructor() {
         this.capacity = 89; //Number of buckets in the hash
         this.load_factor = 0.75 //Increase capacity when buckets are 75% full
+        this.array = [];
     }
 
     hash(key) { //Takes a key and returns a hash that is used to access the appropriate bucket
@@ -14,7 +15,7 @@ class HashMap {
         
         for (let i = 0; i < key.length; i++) {
             hashCode = primeNumber * hashCode + key.charCodeAt(i);
-            hashCode = hashCode % this.buckets;
+            hashCode = hashCode % this.buckets; //this line makes the hashcode smaller
         }
 
         return hashCode;
@@ -23,6 +24,20 @@ class HashMap {
     set(key, value) { 
         //should update the pre-existing value if the key already has a value but set() is called with a new value
         //Deal with collisions! If two keys point to the same bucket, it is a collision!
+
+        let index = this.hash(key);
+
+        if (this.array[index] == undefined) {
+            this.array[index] = value;
+        }
+        else { 
+            //Collision?
+            
+            //If it is not a collision, it is an overwrite operation
+            this.array[index] = value;
+        }
+
+
     
     }
 
