d3ca9d1f2c68e0ccf760e9aa6d750eff0fca263f
BigFatDuck1 <clementho999@gmail.com>
2024-07-08 00:37:52 +0800

    Can generate a list of all legal moves from the start move

--- Knight/knight.js
+++ Knight/knight.js
@@ -17,20 +17,63 @@ function knightMoves(start_array, end_array=[0,0]) {
     let [start_x, start_y] = [...start_array]; //Object destructuring
     let [end_x, end_y] = [...end_array];
 
-    function allMoves(coordinate_array) {
-        let all_legal_moves = [];
-
+    function allMoves(coordinates_array) {
+        
         let generateMove = (x,y) => {
-            x += 1;
-            y += 2;
+            let all_legal_moves = [];
+            //1
+            let x1 = x + 1;
+            let y1 = y + 2;
+            if (legalMove(x1, y1) == true) {
+                all_legal_moves.push([x1,y1]);
+            }
+            //2
+            let x2 = x + 1;
+            let y2 = y - 2;
+            if (legalMove(x2, y2) == true) {
+                all_legal_moves.push([x2,y2]);
+            }
+            //3
+            let x3 = x - 1;
+            let y3 = y - 2;
+            if (legalMove(x3, y3) == true) {
+                all_legal_moves.push([x3,y3]);
+            }
+            //4
+            let x4 = x - 1;
+            let y4 = y + 2;
+            if (legalMove(x4, y4) == true) {
+                all_legal_moves.push([x4,y4]);
+            }
+            //5
+            let x5 = x + 2;
+            let y5 = y + 1;
+            if (legalMove(x5, y5) == true) {
+                all_legal_moves.push([x5,y5]);
+            }
+            //6
+            let x6 = x + 2;
+            let y6 = y - 1;
+            if (legalMove(x6, y6) == true) {
+                all_legal_moves.push([x6,y6]);
+            }
+            //7
+            let x7 = x - 2;
+            let y7 = y - 1;
+            if (legalMove(x7, y7) == true) {
+                all_legal_moves.push([x7,y7]);
+            }
+            //8
+            let x8 = x - 2;
+            let y8 = y + 1;
+            if (legalMove(x8, y8) == true) {
+                all_legal_moves.push([x8,y8]);
+            }
+            return all_legal_moves;
         }
 
-        let move;
-        if (legalMove(move) == true) {
-            all_legal_moves.push(move);
-        }
+        return generateMove(coordinates_array[0], coordinates_array[1]);
 
-        return all_legal_moves;
     }
 
     function legalMove(x, y) {
@@ -49,14 +92,17 @@ function knightMoves(start_array, end_array=[0,0]) {
             return true;
         }
     }
-
-    return legalMove(start_x, start_y);
+    
+    return allMoves([start_x, start_y]);
 }
 
-let test_true = knightMoves([1,1]);
-let test_false = knightMoves([9,9]);
-let test_false2 = knightMoves([-2, 3]);
+// let test_true = knightMoves([1,1]);
+// let test_false = knightMoves([9,9]);
+// let test_false2 = knightMoves([-2, 3]);
 
-console.log(test_true);
-console.log(test_false);
-console.log(test_false2);
+// console.log(test_true);
+// console.log(test_false);
+// console.log(test_false2);
+
+let test = knightMoves([0,0]);
+console.log(test);
